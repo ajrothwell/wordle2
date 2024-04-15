68692ce8725e0e24c3cc92e2ace48cd3c64ef342
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2024-04-15 16:38:58 -0400

    render victory message

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 defineProps<{
-  msg: string
+  wordOfTheDay: string
 }>()
 </script>
 
 <template>
-  <p>{{ msg }}</p>
+  <input type='text'>
+  <p>You won!</p>
+  <!-- <p>{{ wordOfTheDay }}</p> -->
 </template>
 
