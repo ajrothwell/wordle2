a0804b9ed91e32286906e04a886dc495baa8a430
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2024-04-16 09:23:15 -0400

    Extract GuessInput into dedicated component

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { VICTORY_MESSAGE, DEFEAT_MESSAGE, WORD_SIZE } from '@/settings'
+import { VICTORY_MESSAGE, DEFEAT_MESSAGE } from '@/settings'
 import englishWords from "@/englishWordsWith5Letters.json"
-import { ref, computed, watchEffect } from "vue"
+import { ref } from "vue"
+import GuessInput from "@/components/GuessInput.vue"
 
 defineProps({
   wordOfTheDay: {
@@ -10,56 +11,12 @@ defineProps({
   }
 })
 
-const guessInProgress = ref<string|null>(null)
 const guessSubmitted = ref("")
-
-// if you want to make a WRITABLE computed ref, instead of passing a single callback
-// we pass an object that has 2 callbacks inside of it - a getter and a setter
-const formattedGuessInProgress = computed<string>({
-  get() {
-    // ?? is the null coeleascing operator, which means "if the value on the left is null or undefined, use the value on the right"
-    return guessInProgress.value ?? ""
-  },
-  set(rawValue: string) {
-    // guessInProgress.value = null
-    
-    guessInProgress.value = rawValue
-      // .slice(0, WORD_SIZE)
-      // .toUpperCase()
-      // .replace(/[^A-Z]+/gi, "")
-      // the [] allow me to represent a single character of whatever set I want to define inside the brackets
-      // the ^ meant "not", so it's saying "replace anything that's not A-Z with nothing"
-      // the + means I'm looking for one or more occurrences of a character that matches this set
-      // the g means global, and it makes the regex not return after the first match
-      // the i means case-insensitive
-    
-    // console.log('rawValue:', rawValue, 'guessInProgress.value:', guessInProgress.value)
-  }
-})
-
-watchEffect(() => {
-formattedGuessInProgress.value =
-formattedGuessInProgress.value
-?.slice(0, WORD_SIZE)
-.toUpperCase()
-.replace(/[^A-ZЁА-Я]+/gi, "") ?? "";
-});
-
-function onSubmit() {
-  if(!englishWords.includes(formattedGuessInProgress.value)) {
-    return;
-  }
-  guessSubmitted.value = formattedGuessInProgress.value
-}
 
 </script>
 
 <template>
-  <input
-    v-model="formattedGuessInProgress"
-    type='text'
-    @keydown.enter="onSubmit"
-  >
+  <guess-input @guess-submitted="guess => guessSubmitted = guess"/>
   <p
     v-if="guessSubmitted.length > 0"
     v-text="guessSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE"
