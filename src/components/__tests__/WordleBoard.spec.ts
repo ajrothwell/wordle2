e5743632877f871991217840ec087c5da8895cdc
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2024-04-16 15:50:29 -0400

    Extract constant

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -1,6 +1,6 @@
 import { mount } from '@vue/test-utils'
 import WordleBoard from '../WordleBoard.vue'
-import { VICTORY_MESSAGE, DEFEAT_MESSAGE, WORD_SIZE } from '../../settings'
+import { VICTORY_MESSAGE, DEFEAT_MESSAGE, WORD_SIZE, MAX_GUESSES_COUNT } from '../../settings'
 import { beforeEach } from "vitest"
 import { before } from 'node:test'
 
@@ -38,8 +38,8 @@ describe('WordleBoard', () => {
       { numberOfGuesses: 3, shouldSeeDefeatMessage: false },
       { numberOfGuesses: 4, shouldSeeDefeatMessage: false },
       { numberOfGuesses: 5, shouldSeeDefeatMessage: false },
-      { numberOfGuesses: 6, shouldSeeDefeatMessage: true },
-    ])("a defeat message should appear if the player makes incorrect guesses 6 times in a row", ({numberOfGuesses, shouldSeeDefeatMessage}) => {
+      { numberOfGuesses: MAX_GUESSES_COUNT, shouldSeeDefeatMessage: true },
+    ])(`a defeat message should appear if the player makes incorrect guesses ${MAX_GUESSES_COUNT} times in a row`, ({numberOfGuesses, shouldSeeDefeatMessage}) => {
       test(`therefore for ${numberOfGuesses} guess(es), a defeat message should ${shouldSeeDefeatMessage? "" : "not"} appear`, async() => {
         for (let i=0; i<numberOfGuesses; i++) {
           await playerSubmitsGuess("WRONG")
