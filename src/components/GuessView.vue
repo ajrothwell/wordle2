bbb3b8c9b9024f2a68e075a6933cb45980793406
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2024-04-17 11:51:57 -0400

    display feedback for incorrect letter guesses

--- src/components/GuessView.vue
+++ src/components/GuessView.vue
@@ -3,7 +3,16 @@ import { WORD_SIZE } from '@/settings'
 import englishWords from "@/englishWordsWith5Letters.json"
 import { ref, computed, watchEffect } from "vue"
 
-withDefaults(defineProps<{guess: string, shouldFlip?: boolean}>(), {shouldFlip: false})
+// withDefaults(defineProps<{guess: string, shouldFlip?: boolean}>(), {shouldFlip: false})
+const props = defineProps<{guess: string, answer?: string}>()
+
+function getFeedback(letterPosition: number) {
+  if (!props.answer) {
+    return null;
+  }
+
+  return props.answer[letterPosition] === props.guess[letterPosition] ? 'correct' : 'incorrect';
+}
 
 </script>
 
@@ -12,8 +21,8 @@ withDefaults(defineProps<{guess: string, shouldFlip?: boolean}>(), {shouldFlip: 
     <li v-for="(letter, index) in guess.padEnd(WORD_SIZE, ' ')"
         :key="`${letter}-${index}`"
         :data-letter="letter"
-        :data-letter-feedback="shouldFlip ? 'correct' : null"
-        :class="{'with-flips': shouldFlip}"
+        :data-letter-feedback="getFeedback(index)"
+        :class="{'with-flips': answer}"
         class="letter"
         v-text="letter"/>
   </ul>
